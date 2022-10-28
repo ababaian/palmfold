5d6b7340d827fb57419e69c519fde56ef3b7744b
ababaian <artem@rRNA.ca>
2022-10-28 14:43:48 -0400

    Completed verbose logging

--- palmfold.py
+++ palmfold.py
@@ -7,6 +7,9 @@ import logging as log
 
 class PalmStructs:
 
+    # Initialization Routine
+    # Ensures ./pol dir-tree is populated
+    # with the correct files (palmprint pdb, pdb, fasta)
     def __init__(self, polpath):
         # Check if the .pol directory exists
         if not path.exists(polpath):
@@ -81,29 +84,55 @@ class PalmStructs:
 
         self.all_domains = self.rdrps + self.xdxps
 
-    def align(self, directory, name, out_tsv, rdrp_threshold):
-        pdb_file = [file for file in listdir(directory) if file.startswith(f"{name}") and file.endswith(".pdb")]
+    # Classification Routine
+    # Runs TMalign of input against all palmprint pdb
+    # Extracts scores and re-aligns if a RdRp palmprint detected
+    def align(self, pdbpath, name, out_tsv, tm_threshold):
+        # TODO: this looks like an inefficient way to find files in path
+        # it introduces a bug where two files "ABC.pdb" and "ABCD.pdb" will
+        # have the same start and endings, and cause multiple returns and skip
+        # of ABC.pdb
+        pdb_file = [file for file in listdir(pdbpath) if file.startswith(f"{name}") and file.endswith(".pdb")]
         if len(pdb_file) == 0:
-            print(f"No pdb file found for molecule {name}", file=stderr)
+            log.warning("No <input>.pdb file found  %s", name)
             return
         elif len(pdb_file) > 1:
-            print(f"Multiple pdb file found for molecule {name}.", file=stderr)
-            print(f"Abiguous situation, skip molecule", file=stderr)
+            log.warning("Multiple <input>.pdb files found for %s", name)
+            log.warning("Abiguous situation, skip molecule")
             return
-        pdb_file = path.join(directory, pdb_file[0])
+        pdb_file = path.join(pdbpath, pdb_file[0])
 
+        # Reset maximum scores observed
         max_rdrp_score = max_xdxp_score = 0
         max_rdrp = max_xdxp = None
+
+        # Initialize output TSV file
+        log.info("  Writing tsv output to %s", out_tsv)
         with open(out_tsv, "w") as out:
-            print("PDBchain1\tPDBchain2\tTM1\tTM2\tRMSD\tID1\tID2\tIDali\tL1\tL2\tLali", file=out)
-            # Serch for the max score with rdrp proteins
+            print(
+                "PDBchain1\tPDBchain2\tTM1\tTM2\t" +
+                "RMSD\tID1\tID2\tIDali\tL1\tL2\tLali",
+                file=out
+                )
+
+            # TODO: these should be wrapped in functions to not repeat code
+            # Run RdRp-palmprint TMalign and identify max_RdRp_Score
+            log.info("  Run TMalign against RdRp palmprints")
             for domain in self.rdrps:
-                # Run TM-Align
+                # TMalign command
                 tmalign_cmd = f"TMalign -outfmt 2 {pdb_file} {path.join(self.polpath, 'palmprint', f'{domain}.pdb')}"
-                ret_val = subprocess.run(tmalign_cmd.split(" "), capture_output=True, text=True, close_fds=False)
-                # Parse the output
+                log.info("    %s", tmalign_cmd)
+
+                # Runs TMalign and captures output
+                ret_val = subprocess.run(
+                    tmalign_cmd.split(" "),
+                    capture_output=True,
+                    text=True,
+                    close_fds=False)
+                # Parse the captured output for scores
                 values = ret_val.stdout.split("\n")[1]
                 values = [name, domain] + [float(x) for x in values.split("\t")[2:]]
+                # Append TMalign output to TSV output
                 print("\t".join([str(v) for v in values]), file=out)
                 # Save best scores
                 score = values[3]
@@ -111,14 +140,23 @@ class PalmStructs:
                     max_rdrp_score = score
                     max_rdrp = domain
 
-            # Serch for the max score with xdxp proteins
+            # Run XdXp-palmprint TMalign and identify max_XdXp_Score
+            log.info("  Run TMalign against XdXp palmprints")
             for domain in self.xdxps:
                 # Run TM-Align
                 tmalign_cmd = f"TMalign -outfmt 2 {pdb_file} {path.join(self.polpath, 'palmprint', f'{domain}.pdb')}"
-                ret_val = subprocess.run(tmalign_cmd.split(" "), capture_output=True, text=True, close_fds=False)
-                # Parse the output
+                log.info("    %s", tmalign_cmd)
+                
+                # Runs TMalign and captures output
+                ret_val = subprocess.run(
+                    tmalign_cmd.split(" "),
+                    capture_output=True,
+                    text=True,
+                    close_fds=False)
+                # Parse the captured output for scores
                 values = ret_val.stdout.split("\n")[1]
                 values = [name, domain] + [float(x) for x in values.split("\t")[2:]]
+                # Append TMalign output to TSV output
                 print("\t".join([str(v) for v in values]), file=out)
                 # Save best scores
                 score = values[3]
@@ -127,26 +165,48 @@ class PalmStructs:
                     max_xdxp = domain
 
         # Does maxRdRP_score pass CUTOFF value
-        if max_rdrp_score >= rdrp_threshold:
-            # Does maxRdRP surpass maxXdXP
+        log.info("")
+        log.info("  Max RdRp palmprint: %s - %s", max_rdrp, max_rdrp_score)
+        log.info("  Max XdXp palmprint: %s - %s", max_xdxp, max_xdxp_score)
+        log.info("")
+
+        # RdRp classification
+        # RdRp TMscore above threshold
+        # and RdRp > XdXp model scores
+        if max(max_rdrp_score, max_xdxp_score) >= tm_threshold:
+            log.info("  + Polymerase threshold score reached: %s", tm_threshold)
             if max_rdrp_score > max_xdxp_score:
-                # Realign the protein with the ref
+                # RdRp+ classification
+                log.info("  ++ RdRp-classification for %s", name)
+                log.info("")
+                log.info("  Re-aligning:")
+
+                # Run TMalign to re-align input structure to top-RdRp palmprint
                 realign_cmd = f"TMalign -outfmt 1 {pdb_file} {path.join(self.polpath, 'palmprint', f'{domain}.pdb')} -o {pdb_file}_tmpalign"
+                log.info("  %s", realign_cmd)
                 with open(f"{pdb_file}.tmp", "w") as output:
                     subprocess.run(realign_cmd.split(" "), stdout=output)
+
+                log.info("  Re-aligning:")
+
                 # Get the realign
                 rename(f"{pdb_file}_tmpalign.pdb", f"{pdb_file}_realign.pdb")
+
                 # Get the fastas
                 scriptdir = path.dirname(path.realpath(__file__))
                 subprocess.run(['python3', path.join(scriptdir, "palmgrab.py"), f"{pdb_file}.tmp", f"{pdb_file}.pp.fa", f"{pdb_file}.rc.fa"])
-                for f in listdir(directory):
+                for f in listdir(pdbpath):
                     if "_tmpalign" in f:
-                        remove(path.join(directory, f))
+                        remove(path.join(pdbpath, f))
                 remove(f"{pdb_file}.tmp")
-                remove(pdb_file)
+                #remove(pdb_file)
+            else:
+                # TODO: Add secondary output flag to include re-aligned XdXp outputs
+                log.info("  %s classified as non-RdRp polymerase", name)
 
 
-# Main script routine =====================================
+
+# Initialization routine ==================================
 def main(inputpath, palmprints, threshold):
     # Verify Input Path exists
     if not path.exists(inputpath):
@@ -168,6 +228,7 @@ def main(inputpath, palmprints, threshold):
 
     # Classify the input protein structures
     for prot in names:
+        log.info("")
         log.info("  Analyzing %s...", prot)
         ps.align(inputpath, prot, path.join(inputpath, f"{prot}.tm"), threshold)
 
@@ -187,7 +248,7 @@ if __name__ == "__main__":
     ## TODO: implement output directory
     parser.add_argument(
         '--outpath', '-o', type=str, default='./out',
-        help='Output directory into which output files are created. [./out]'
+        help='TODO: Output directory into which output files are created. [./out]'
         )
     parser.add_argument(
         '--threshold', '-t', type=float, default=0.5,
@@ -205,6 +266,17 @@ if __name__ == "__main__":
     else:
         log.basicConfig(format="%(levelname)s: %(message)s")
 
-    main(args.inputpath, args.palmprints, args.threshold)
+    # Main script routine =================================
+    log.info('''
+      _____      _            __      _     _ 
+     |  __ \    | |          / _|    | |   | |
+     | |__) |_ _| |_ __ ___ | |_ ___ | | __| |
+     |  ___/ _` | | '_ ` _ \|  _/ _ \| |/ _` |
+     | |  | (_| | | | | | | | || (_) | | (_| |
+     |_|   \__,_|_|_| |_| |_|_| \___/|_|\__,_|
+     =========================================
+    ''')
 
-    
+    main(args.inputpath, args.palmprints, args.threshold)
+    log.info("")
+    log.info("palmfold run completed successfully")
